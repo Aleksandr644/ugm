26de3772e88b586ebffb2ff9bb45164e9f28c9a3
Aleksandr644 <u9995844696@yandex.ru>
2024-03-25 21:41:26 +0500

    Переход на psycopg2 -> sqlalchemy -> flask-sqlalchemy + sqlalchemy-stubs(from typing import TYPE_CHECKING)

--- uwsgi/ugm/app/__init__.py
+++ uwsgi/ugm/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-from postgreDB import PGDB
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 csrf = CSRFProtect()
 app = Flask(__name__)
 app.config.from_object('config')
 csrf.init_app(app)
+db = SQLAlchemy(app)
 
-from app import views, models # ! Обязательная строчка, без нее не подлкючаются виды
+from app import views, models # ! Обязательная строчка, без нее не подлкючаются виды, модели
--- uwsgi/ugm/config.py
+++ uwsgi/ugm/config.py
@@ -1,3 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = None
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+HOST='localhost'
+DATABASE='uralgipromash'
+PASSWORD='UGM402'
+USER='postgres'
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
